773e0f9c490fd57c19ac5928ff6cea0465a205d9
Giriirig <91939305+Giriiri-g@users.noreply.github.com>
2026-06-22 16:33:50 +0530

    Refactoring: Logout endpoint now under AuthController

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -63,6 +63,22 @@ public class AuthController : ControllerBase
 
         return Ok(new { token = token });
     }
+
+    [HttpPost("logout")]
+    [Authorize]
+    public IActionResult Logout()
+    {
+        Response.Cookies.Delete(
+            "auth_token",
+            new Microsoft.AspNetCore.Http.CookieOptions
+            {
+                HttpOnly = true,
+                SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax
+            }
+        );
+
+        return Ok();
+    }
 }
 
 
